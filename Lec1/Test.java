366c763530cef7bd8777eecdb5d33bc3a59d19a2
monukumar98 <monu012as@gmail.com>
2021-05-31 23:29:28 +0530

    Lec1

---
+++ Lec1/Test.java
@@ -0,0 +1,14 @@
+package Lec1;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+     System.out.println("welcome to CB");
+     System.out.print("welcome to CB");
+
+	}
+
+}
+    
---
+++ Lec1/Variables_Demo.java
@@ -0,0 +1,22 @@
+package Lec1;
+
+public class Variables_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=9;
+		System.out.println("X is "+x);
+		int a=6;
+		int b =5;
+		int c = a+b;
+		System.out.println(c);
+		int d = a-b;
+		System.out.println(d);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(6%4);
+		 
+
+	}
+
+}
